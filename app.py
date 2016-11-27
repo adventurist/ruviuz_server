9c640c8fd4350a97396f10ae5f44cd9f45506bce
logicp <emmanuel@openconcept.ca>
2016-11-27 12:46:59 -0500

    Improved Roof aspect of API

--- app.py
+++ app.py
@@ -1,6 +1,8 @@
 from flask import Flask, abort, jsonify, url_for, render_template, g, request
+from flask.json import JSONEncoder
 from flask_sqlalchemy import SQLAlchemy
 from flask_httpauth import HTTPBasicAuth
+import decimal
 from passlib.apps import custom_app_context as pwd_context
 from itsdangerous import (TimedJSONWebSignatureSerializer as Serializer, BadSignature, SignatureExpired)
 
@@ -10,6 +12,18 @@ app.config['SQLALCHEMY_DATABASE_URI'] = 'postgresql://ruvadmin:ge9BQ7fT8bVBgm1B@
 app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = True
 db = SQLAlchemy(app)
 auth = HTTPBasicAuth()
+
+
+class MJSONEncoder(JSONEncoder):
+
+    def default(self, obj):
+        if isinstance(obj, decimal.Decimal):
+            # Convert decimal instances to strings.
+            return str(obj)
+        return super(MJSONEncoder, self).default(obj)
+
+
+app.json_encoder = MJSONEncoder
 
 
 class User(db.Model):
@@ -51,6 +65,14 @@ class Roof(db.Model):
     width = db.Column(db.DECIMAL(10, 3))
     slope = db.Column(db.Float)
     price = db.Column(db.DECIMAL(10, 2))
+    address = db.Column(db.VARCHAR(255))
+
+    def serialize(self):
+        return {
+            'id': self.id,
+            'address': self.address,
+            'price': self.price,
+        }
 
 
 @auth.verify_password
@@ -73,7 +95,6 @@ def index():
 
 
 @app.route('/login', methods=['POST'])
-
 def login():
     if request.headers['content-Type'] == 'application/x-www-form-urlencoded':
         email = request.form['email']
@@ -120,6 +141,37 @@ def login():
         return jsonify({'email': user.email}), 201, {'Location': url_for('get_user', id=user.id, _external=True)}
 
 
+@app.route('/roof/add', methods=['POST'])
+@auth.login_required
+def add_roof():
+    print 'Requesting roof addition'
+    if request.headers['Content-Type'] == 'application/json':
+        print(request.json)
+        length = request.json.get('length')
+        width = request.json.get('width')
+        slope = request.json.get('slope')
+        address = request.json.get('address')
+        price = request.json.get('price')
+
+        if length is None or width is None or slope is None or address is None or price is None:
+            print 'Something not set'
+            abort(400)
+        if Roof.query.filter_by(address=address).first() is not None:
+            roof = Roof(address=address, price=price)
+            print 'Found a roof'
+            if roof is not None:
+                print 'Roof is not None'
+                print str(roof.serialize())
+                return jsonify({'Roof': roof.serialize()}), 201
+        print 'Make new roof'
+        roof = Roof(address=address, length=length, width=width, slope=slope, price=price)
+        db.session.add(roof)
+        db.session.commit()
+        print 'Created roof==> ' + roof
+        return jsonify({'Roof': roof.serialize()}), 201, {
+            'Location': url_for('get_roof', address=roof.address, _external=True)}
+
+
 @app.route('/users/<int:id>')
 @auth.login_required
 def get_user(id):
@@ -127,6 +179,16 @@ def get_user(id):
     if not user:
         abort(400)
     return jsonify({'email': user.email, 'password': user.password_hash})
+
+
+@app.route('/roofs/<string:address>')
+@auth.login_required
+def get_roof(address):
+    roof = Roof.query.get(address=address)
+    if not roof:
+        abort(400)
+    return jsonify({'Roof': roof.serialize()})
+
 
 @app.route('/token')
 @auth.login_required
@@ -139,6 +201,7 @@ def get_auth_token():
 @auth.login_required
 def get_resource():
     return jsonify({'data': 'Hello, %s!' % g.user.email})
+
 
 if __name__ == '__main__':
     app.debug = True
