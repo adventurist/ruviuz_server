9384fbd751dae0411e136054c8f7cd0f6daa5d63
logicp <emmanuel@openconcept.ca>
2016-12-23 23:15:03 -0500

    Update debug

--- app.py
+++ app.py
@@ -346,7 +346,7 @@ def update_roof(id):
                     print 'Adding new file for RID==>' + str(id) + '\n with Filename==>' + filename
                     files_not_found += '{' + str(i) + ':"' + filename + '"},'
         files_not_found = '{"filesToSend":' + files_not_found[:-1] + ']' + '}'
-        return jsonify({'filesToSend', files_not_found})
+        return files_not_found
             # for key in filesjson:
             #     # get filename from file
             #     file = filesjson[key]
