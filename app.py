33f09d8e7deecfdd6da4468d9abaa3762156d82d
logicp <emmanuel@openconcept.ca>
2017-02-06 12:11:52 -0500

    Fixing customers in roofs/all

--- app.py
+++ app.py
@@ -461,7 +461,7 @@ def get_roofs():
         if cQuery.count() > 0:
             cResult = cQuery.first()
             mJson = mJson[:-1]
-            mJson += ',"customer":"' + cResult.first + ' ' + cResult.last + '",'
+            mJson += ',"customer":"' + cResult.first + ' ' + cResult.last + '"'
         if fQuery.count() > 0:
             fcount = 0
             fileResult = fQuery.all()
