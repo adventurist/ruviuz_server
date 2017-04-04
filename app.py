d12701a1f1ecc2b4844ba974530d3a4f2d5ac866
logicp <emmanuel@openconcept.ca>
2017-04-03 22:35:45 -0400

    FIxing bug in roofs/delete

--- app.py
+++ app.py
@@ -624,7 +624,7 @@ def delete_roof(rid):
         db.session.commit()
         db.session.flush()
 
-        return jsonify({'RuvDelete', 'Success', 'Deleted', str(rid)}), 204
+        return jsonify({'RuvDelete': 'Success', 'Deleted': str(rid)}), 204
 
     except exc.SQLAlchemyError as e:
         logging.warning("Unable to delete roof and its children")
